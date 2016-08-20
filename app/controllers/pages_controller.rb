fec88a98f1f1eab5cc07495c85b166458d32856a
Andrew Graham-Yooll <andrewgy8@gmail.com>
2016-08-20 14:21:14 +0200

    Basic static pages layout

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def research
+  end
+
+  def books
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def blog
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get research" do
+    get pages_research_url
+    assert_response :success
+  end
+
+  test "should get books" do
+    get pages_books_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+  end
+
+  test "should get blog" do
+    get pages_blog_url
+    assert_response :success
+  end
+
+end
