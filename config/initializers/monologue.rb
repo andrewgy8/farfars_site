7033f2b96e9c0e2f25b7e8658bf2a1e030f9674d
Andrew Graham-Yooll <andrewgy8@gmail.com>
2016-08-27 13:18:15 +0200

    The links have been cut down

--- config/initializers/monologue.rb
+++ config/initializers/monologue.rb
@@ -17,7 +17,7 @@ Monologue.config do |config|
   config.facebook_like_locale = "en_US" # "fr_CA"
   config.google_plusone_locale = "en"
 
-  # config.layout               = "layouts/application"
+  config.layout               = "layouts/application"
 
   # ANALYTICS
   # config.gauge_analytics_site_id = "YOUR COGE FROM GAUG.ES"
@@ -28,7 +28,7 @@ Monologue.config do |config|
 
   #SOCIAL
   config.twitter_username = "myhandle"
-  config.facebook_url = "https://www.facebook.com/myhandle"
+  config.facebook_url = "https://www.facebook.com/andrewgy8"
   config.facebook_logo = 'logo.png'
   config.google_plus_account_url = "https://plus.google.com/u/1/.../posts"
   config.linkedin_url = "http://www.linkedin.com/in/myhandle"
