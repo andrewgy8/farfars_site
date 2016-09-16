97be2511fab51abd3152817a19a0593edaeadb32
Andrew Graham-Yooll <andrewgy8@gmail.com>
2016-09-16 15:26:34 +0200

    Fixed the title of the site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = "Andrew's Sample Site"
+    base_title = "Fred Graham-Yooll"
     if page_title.empty?
       base_title
     else
